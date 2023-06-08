16da15ac04fedd0b463fe7e1bae40fe7702c6282
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-06-08 14:32:11 -0700

    Auto update

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-// Get the fetch button element
-const fetchButton = document.getElementById('fetchButton');
+// Fetch the text file containing the URLs
+fetch('https://raw.githubusercontent.com/HttpAnimation/SteamFree/main/game.txt')
+    .then(response => response.text())
+    .then(data => {
+        // Split the text into an array of URLs
+        const urls = data.trim().split('\n');
 
-// Add click event listener to the fetch button
-fetchButton.addEventListener('click', () => {
-    // Fetch the text file containing the URLs
-    fetch('https://raw.githubusercontent.com/HttpAnimation/SteamFree/main/game.txt')
-        .then(response => response.text())
-        .then(data => {
-            // Split the text into an array of URLs
-            const urls = data.trim().split('\n');
+        // Get the button container element
+        const buttonContainer = document.getElementById('buttonContainer');
 
-            // Create buttons for each URL
-            urls.forEach(url => {
-                const button = document.createElement('button');
-                button.textContent = url;
-                button.addEventListener('click', () => {
-                    window.open(url, '_blank');
-                });
-                document.body.appendChild(button);
+        // Create buttons for each URL
+        urls.forEach(url => {
+            const button = document.createElement('button');
+            button.textContent = url;
+            button.addEventListener('click', () => {
+                window.open(url, '_blank');
             });
-        })
-        .catch(error => {
-            console.error('Failed to fetch URLs:', error);
+            buttonContainer.appendChild(button);
         });
-});
+    })
+    .catch(error => {
+        console.error('Failed to fetch URLs:', error);
+    });
