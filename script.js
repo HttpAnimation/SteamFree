09e54d9a5137e767b7cbdb2ff52515fc148a3cd8
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-06-08 14:45:02 -0700

    old script.js until I fix it

--- script.js
+++ script.js
@@ -10,11 +10,8 @@ fetch('https://raw.githubusercontent.com/HttpAnimation/SteamFree/main/game.txt')
 
         // Create buttons for each URL
         urls.forEach(url => {
-            // Extract the name from the URL and replace underscores with spaces
-            const name = url.split('/').pop().replace(/_/g, ' ').replace(/\.(?:html?|php)$/i, '');
-
             const button = document.createElement('button');
-            button.textContent = name;
+            button.textContent = url;
             button.addEventListener('click', () => {
                 window.open(url, '_blank');
             });
