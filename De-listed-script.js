7e1627f7fcf45130a374940982e06fb149732709
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-06-08 21:39:42 -0700

    Adds a lot of stuff

---
+++ De-listed-script.js
@@ -0,0 +1,23 @@
+// Fetch the text file containing the URLs
+fetch('https://raw.githubusercontent.com/HttpAnimation/SteamFree/main/delisted.txt')
+    .then(response => response.text())
+    .then(data => {
+        // Split the text into an array of URLs
+        const urls = data.trim().split('\n');
+
+        // Get the button container element
+        const buttonContainer = document.getElementById('buttonContainer');
+
+        // Create buttons for each URL
+        urls.forEach(url => {
+            const button = document.createElement('button');
+            button.textContent = url;
+            button.addEventListener('click', () => {
+                window.open(url, '_blank');
+            });
+            buttonContainer.appendChild(button);
+        });
+    })
+    .catch(error => {
+        console.error('Failed to fetch URLs:', error);
+    });
